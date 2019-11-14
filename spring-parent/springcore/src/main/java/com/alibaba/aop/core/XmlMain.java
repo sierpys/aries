d7a6b3644661510f2bb8410135348066ea3edc6c
思尔 <sier.pys@alibaba-inc.com>
2019-11-14 10:55:31 +0800

    accessor

--- spring-parent/springcore/src/main/java/com/alibaba/aop/core/XmlMain.java
+++ spring-parent/springcore/src/main/java/com/alibaba/aop/core/XmlMain.java
@@ -1,19 +1,35 @@
 package com.alibaba.aop.core;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.sun.org.apache.bcel.internal.generic.LADD;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import javax.swing.plaf.IconUIResource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sier.pys 10/27/18
  */
 public class XmlMain {
-    public static void main(String[] args) {
-//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AopConfiguration.class);
-//        Printer printer = applicationContext.getBean(Printer.class);
-//        printer.print("hello world");
+    private static Flux<String> getZipDescFlux() {
+        String desc = "Zip two sources together, that is to say wait for all the sources to emit one element and combine these elements once into a Tuple2.";
+        return Flux.fromArray(desc.split("\\s+"));  // 1
+    }
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application.xml");
-        SourceDataProvider dataProvider = context.getBean(SourceDataProvider.class);
-        dataProvider.printSource();
+    private static String getStringSync() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Hello, Reactor!";
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Mono.fromCallable(() -> getStringSync()).subscribeOn(Schedulers.elastic()).subscribe(System.out::println);
+        Thread.sleep(10000);
+
     }
 }
